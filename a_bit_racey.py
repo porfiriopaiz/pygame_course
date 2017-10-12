fe12345486f595dbd1112f51c3d9470f349c883c
Porfirio A. Páiz Carrasco <porfiriopaiz@gmail.com>
2017-10-12 14:54:12 -0600

    Add changes based on 2 - Display Images.

--- a_bit_racey.py
+++ a_bit_racey.py
@@ -1,9 +1,25 @@
 import pygame
 
 pygame.init()
-gameDisplay = pygame.display.set_mode((800, 600))
+
+display_width = 800
+display_height = 600
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+
+gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('A bit Racey')
 clock = pygame.time.Clock()
+
+carImg = pygame.image.load("racecar.png")
+
+def car(x, y):
+    gameDisplay.blit(carImg, (x, y))
+
+x = (display_width * 0.45)
+y = (display_height * 0.8)
 
 crashed = False
 
@@ -14,6 +30,9 @@ while not crashed:
             crashed = True
 
         print(event)
+
+    gameDisplay.fill(white)
+    car(x, y)
 
     pygame.display.update()
     clock.tick(60)
