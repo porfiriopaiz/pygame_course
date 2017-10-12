341b45a81134d7dd570bfe5f062a1085d3bcdab3
Porfirio A. Páiz Carrasco <porfiriopaiz@gmail.com>
2017-10-12 16:55:17 -0600

    Add changes based on 5 - Display Text on Screen.

--- a_bit_racey.py
+++ a_bit_racey.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 
 pygame.init()
 
@@ -20,6 +21,25 @@ carImg = pygame.image.load("racecar.png")
 def car(x, y):
     gameDisplay.blit(carImg, (x, y))
 
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+def message_display(text):
+    largeText = pygame.font.Font('freesansbold.ttf', 115)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((display_width / 2), (display_height / 2))
+    gameDisplay.blit(TextSurf, TextRect)
+
+    pygame.display.update()
+
+    time.sleep(2)
+
+    game_loop()
+
+def crash():
+    message_display('You Crashed.')
+
 def game_loop():
 
     x = (display_width * 0.45)
@@ -33,7 +53,8 @@ def game_loop():
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                gameExit = True
+                pygame.quit()
+                quit()
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
@@ -51,7 +72,7 @@ def game_loop():
         car(x, y)
 
         if x > display_width - car_width or x < 0:
-            gameExit = True
+            crash()
 
         pygame.display.update()
         clock.tick(60)
