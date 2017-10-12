446cbf9f78f0066716026b3b47337bf0bd2b4368
Porfirio A. Páiz Carrasco <porfiriopaiz@gmail.com>
2017-10-12 15:14:33 -0600

    Add changes based on 3 - Moving an Image.

--- a_bit_racey.py
+++ a_bit_racey.py
@@ -21,6 +21,8 @@ def car(x, y):
 x = (display_width * 0.45)
 y = (display_height * 0.8)
 
+x_change = 0
+
 crashed = False
 
 while not crashed:
@@ -29,7 +31,17 @@ while not crashed:
         if event.type == pygame.QUIT:
             crashed = True
 
-        print(event)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x_change = -5
+            elif event.key == pygame.K_RIGHT:
+                x_change = 5
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                x_change = 0
+
+    x += x_change
 
     gameDisplay.fill(white)
     car(x, y)
