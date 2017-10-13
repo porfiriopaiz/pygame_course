b3dbf331671f619855b03e5e7e1918c0a08735bd
Porfirio A. Páiz Carrasco <porfiriopaiz@gmail.com>
2017-10-13 16:50:14 -0600

    Add changes based on 6 - Drawing Objects _ moving objects.

--- a_bit_racey.py
+++ a_bit_racey.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+import random
 
 pygame.init()
 
@@ -17,6 +18,9 @@ pygame.display.set_caption('A bit Racey')
 clock = pygame.time.Clock()
 
 carImg = pygame.image.load("racecar.png")
+
+def things(thingx, thingy, thingw, thingh, color):
+    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
 
 def car(x, y):
     gameDisplay.blit(carImg, (x, y))
@@ -47,6 +51,12 @@ def game_loop():
 
     x_change = 0
 
+    thing_startx = random.randrange(0, display_width)
+    thing_starty = -600
+    thing_speed = 7
+    thing_width = 100
+    thing_height = 100
+
     gameExit = False
 
     while not gameExit:
@@ -67,12 +77,18 @@ def game_loop():
                     x_change = 0
 
         x += x_change
-
         gameDisplay.fill(white)
+
+        things(thing_startx, thing_starty, thing_width, thing_height, black)
+        thing_starty += thing_speed
         car(x, y)
 
         if x > display_width - car_width or x < 0:
             crash()
+
+        if thing_starty > display_height:
+            thing_starty = 0 - thing_height
+            thing_startx = random.randrange(0, display_width)
 
         pygame.display.update()
         clock.tick(60)
